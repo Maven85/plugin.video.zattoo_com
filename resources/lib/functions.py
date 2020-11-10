5013c05c6fd4962be4d8e2a61f70dfde6bbbde31
Maven85 <janis.semper@googlemail.com>
2020-11-10 08:48:52 +0100

    import translatePath from xbmcvfs for Kodi Matrix

--- resources/lib/functions.py
+++ resources/lib/functions.py
@@ -6,8 +6,9 @@ from kodi_six.utils import py2_decode
 def warning(text, title='Zattoo Live TV', time=4500, exit=False):
     import xbmc
     import xbmcaddon
+    import xbmcvfs
     import os.path
-    icon = py2_decode(os.path.join(xbmc.translatePath(xbmcaddon.Addon('plugin.video.zattoo_com').getAddonInfo('path')), 'icon.png'))
+    icon = py2_decode(os.path.join(xbmcvfs.translatePath(xbmcaddon.Addon('plugin.video.zattoo_com').getAddonInfo('path')), 'icon.png'))
     xbmc.executebuiltin('Notification(%s, %s, %d, %s)' % (title, text, time, icon))
     if exit:
         import sys
