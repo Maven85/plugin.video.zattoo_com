9dc37fbf86a1f10062b1885f5b2ad9c87d417a70
Maven85 <janis.semper@googlemail.com>
2021-02-05 06:48:34 +0100

    fixed playback error when session expired

--- resources/lib/watch.py
+++ resources/lib/watch.py
@@ -3,16 +3,25 @@ from __future__ import unicode_literals
 import json
 
 try:
+    from urllib.error import HTTPError
     from urllib.parse import urljoin
     from urllib.request import urlopen
 except:
-    from urllib2 import urlopen
+    from urllib2 import urlopen, HTTPError
     from urlparse import urljoin
 
 
 def get_playlist_url(cid, SESSION):
     from .api import get_json_data
-    json_data = get_json_data('https://zattoo.com/zapi/watch/live/%s' % cid, SESSION, {'stream_type':'hls', 'https_watch_urls':True})
+    try:
+        json_data = get_json_data('https://zattoo.com/zapi/watch/live/%s' % cid, SESSION, {'stream_type':'hls', 'https_watch_urls':True})
+    except HTTPError:
+        from .api import login
+        login()
+        import xbmcaddon
+        addon = xbmcaddon.Addon(id='plugin.video.zattoo_com')
+        SESSION = addon.getSetting('session')
+        json_data = get_json_data('https://zattoo.com/zapi/watch/live/%s' % cid, SESSION, {'stream_type':'hls', 'https_watch_urls':True})
     return json.loads(json_data)['stream']['url']
 
 
